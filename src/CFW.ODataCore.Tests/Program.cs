bc257d7ec89df20547a35ee54ca0aeb931a65898
dathuynh223 <dat.ht@logixtek.com>
2024-12-11 12:16:01 +0700

    Change default provider to sqlite

--- src/CFW.ODataCore.Tests/Program.cs
+++ src/CFW.ODataCore.Tests/Program.cs
@@ -1,14 +1,23 @@
 using CFW.ODataCore;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.ConfigureWarnings(warnings =>
-        warnings.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.PendingModelChangesWarning));
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//{
+//    options.ConfigureWarnings(warnings =>
+//        warnings.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.PendingModelChangesWarning));
 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//});
+
+var sqliteConnection = new SqliteConnection("DataSource=:memory:");
+sqliteConnection.Open();
+builder.Services.AddDbContext<AppDbContext>((s, option) =>
+{
+    option.UseSqlite(sqliteConnection);
+    option.ConfigureWarnings(x => x.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.AmbientTransactionWarning));
 });
 
 builder.Services
@@ -17,5 +26,9 @@ builder.Services
 var app = builder.Build();
 
 app.UseGenericODataEndpoints();
+
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+db.Database.EnsureCreated();
 
 app.Run();
